732c4dce3cdc451e339761657fb0c93baff0e77c
zijiao <zijiao@mogujie.com>
2016-08-24 18:22:25 +0800

    fix

--- app/src/main/java/com/puke/buildergeneator/MainActivity.java
+++ app/src/main/java/com/puke/buildergeneator/MainActivity.java
@@ -9,10 +9,5 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Dog dog = new DogBuilder()
-                .configureAge(1)
-                .configureSex("男")
-                .configureCustomName("名字")
-                .build();
     }
 }
